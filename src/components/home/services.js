055b46df5ec346a518a76fb4e780b10b46239f54
jakubmachac <jakub.machac@beafuture.com>
2024-12-28 21:09:49 +0100

    images sources fix

--- src/components/home/services.js
+++ src/components/home/services.js
@@ -2,21 +2,21 @@ export const services = [
   {
     title: "Fyzioterapie",
     text: "Nabízí individuální přístup k terapii, který zahrnuje vyšetření, manuální ošetření a návrh cvičení pro dosažení trvalých změn. Kombinuje různé terapeutické přístupy, včetně jógy a kraniosakrální terapie, a poskytuje zázemí i pro klienty s dětmi.",
-    image: "../../../public/service1.png"
+    image: "/service1.png"
   },
   {
     title: "Kraniosakrální terapie",
     text: "Nabízí individuální přístup k terapii, který zahrnuje vyšetření, manuální ošetření a návrh cvičení pro dosažení trvalých změn. Kombinuje různé terapeutické přístupy, včetně jógy a kraniosakrální terapie, a poskytuje zázemí i pro klienty s dětmi.",
-    image: "../../../public/service1.png"
+    image: "/service1.png"
   },
   {
     title: "Terapie s prvky senzorické integrace",
     text: "Nabízí individuální přístup k terapii, který zahrnuje vyšetření, manuální ošetření a návrh cvičení pro dosažení trvalých změn. Kombinuje různé terapeutické přístupy, včetně jógy a kraniosakrální terapie, a poskytuje zázemí i pro klienty s dětmi.",
-    image: "../../../public/service1.png"
+    image: "/service1.png"
   },
   {
     title: "Reflexní lymfodrenáž",
     text: "Nabízí individuální přístup k terapii, který zahrnuje vyšetření, manuální ošetření a návrh cvičení pro dosažení trvalých změn. Kombinuje různé terapeutické přístupy, včetně jógy a kraniosakrální terapie, a poskytuje zázemí i pro klienty s dětmi.",
-    image: "../../../public/service1.png"
+    image: "/service1.png"
   },
 ]
