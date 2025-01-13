1ed773572e4a310eddb606ef12eada2032b1460e
jakubmachac <jakub.machac@beafuture.com>
2025-01-13 16:55:48 +0100

    images changed

--- src/components/home/services.js
+++ src/components/home/services.js
@@ -2,31 +2,31 @@ export const services = [
   {
     title: "Fyzioterapie",
     text: "Hlavním cílem fyzioterapie je obnovení a normalizace pohybů tak, aby nedocházelo k přetížení a bolestem pohybového systému. Základem terapie je správná diagnostika, která se opírá o anamnézu (pohovor s pacientem o průběhu a charakteru jeho obtíží)\n.",
-    image: "/service1.png",
+    image: "/sluzby/fyzioterapie.png",
     path: "fyzioterapie"
   },
   {
     title: "Fyzioterapie pro děti a miminka",
     text: "Fyzioterapie pro děti zahrnuje péči o kojence (0–4 roky) zaměřenou na psychomotorický vývoj, asymetrie, reflux a neurologické obtíže, i pro děti od 5 let, se zaměřením na bolesti, skoliózu, držení těla a grafomotoriku. Součástí je senzorická integrace, která podporuje zpracování smyslových podnětů a motoriku; terapie řeší obtíže formou hry ve speciálním prostředí.",
-    image: "/service1.png",
+    image: "/sluzby/pro-deti.png",
     path: "fyzioterapie-pro-deti"
   },
   {
     title: "Podologie",
     text: "Podologie je specializovaná oblast fyzioterapie zaměřená na diagnostiku, léčbu a prevenci poruch nohou. Cílem je podpora správné funkce plosky nohy a korekce jejích patologií. Využívá podoskop pro analýzu, senzoromotorickou stimulaci, manuální techniky a cvičení pro zlepšení rovnováhy, pohybu a stability.",
-    image: "/service1.png",
+    image: "/sluzby/podologie.png",
     path: "podologie"
   },
   {
     title: "Viscelární terapie",
     text: "Jemná manuální metoda zaměřená na uvolnění napětí vnitřních orgánů, zlepšení jejich funkce a podporu celkového zdraví. Pomáhá při bolestech břicha, trávících problémech, refluxu, gynekologických obtížích, pooperační péči a u dětí na zmírnění stresu a trávících potíží.",
-    image: "/service1.png",
+    image: "/sluzby/viscelarni.png",
     path: "viscelarni-terapie"
   },
   {
     title: "Fyzikální terapie",
     text: "Využívá elektrický proud k léčbě různých diagnóz, jako jsou ortopedické, revmatologické, neurologické a cévní problémy. Pomáhá zmírnit bolest, uvolnit svaly a zlepšit prokrvení. Vysokovýkonový laser proniká do hlubokých tkání, urychluje hojení a zmírňuje bolest bez léků.",
-    image: "/service1.png",
+    image: "/sluzby/fyzikalni.png",
     path: "fyzikalni-terapie"
   },
 ]
